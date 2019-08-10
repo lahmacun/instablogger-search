dfee9c69e86a8b320f89200d5eac20ec02c8a1ed
Mustafa Zahid Efe <zhdefe@gmail.com>
2019-08-10 20:24:32 +0300

    Finished app

---
+++ assets/js/custom.js
@@ -0,0 +1,57 @@
+$(function() {
+    $.ajax({
+        type: "GET",
+        url: "https://zahidefe.net/wp-json/wp/v2/posts?page=1&per_page=10&_embed",
+        success: function(response) {
+            $('.posts').html("");
+            response.forEach(function(post) {
+                var post_image = post._embedded['wp:featuredmedia'] ? '<div class="card-image"><img src="' + post._embedded['wp:featuredmedia']['0'].source_url + '" alt="" style="height: 100%;"></div>' : '';
+                $(".posts").append('<div class="col s12 m6">\n' +
+                    '\t\t\t    <div class="card darken-1">\n' +
+                    post_image +
+                    '\t\t\t\t    <div class="card-content white-text">\n' +
+                    '\t\t\t\t\t    <span class="card-title">' + post.title.rendered + '</span>\n' +
+                    '\t\t\t\t\t    <p>' + post.excerpt.rendered + '</p>\n' +
+                    '\t\t\t\t    </div>\n' +
+                    '\t\t\t\t    <div class="card-action">\n' +
+                    '\t\t\t\t\t    <a href="https://zahidefe.net/?p=' + post.link + '">Yazıyı Oku</a>\n' +
+                    '\t\t\t\t    </div>\n' +
+                    '\t\t\t    </div>\n' +
+                    '\t\t    </div>');
+            })
+        }
+    });
+
+    $('#search-form').on('submit', function(e) {
+        e.preventDefault();
+        var search_term = $('.search').val();
+        if ($.trim(search_term) == '') {
+            var search_url = "https://zahidefe.net/wp-json/wp/v2/posts?page=1&per_page=10&_embed";
+        } else {
+            var search_url = "https://zahidefe.net/wp-json/wp/v2/posts?meta_key=lahmacun_post_code&meta_value=" + search_term + "&_embed";
+        }
+        $('.posts').html('<img src="/assets/img/loading.svg" />');
+        $.ajax({
+            type: "GET",
+            url: search_url,
+            success: function(response) {
+                $('.posts').html("");
+                response.forEach(function(post) {
+                    var post_image = post._embedded['wp:featuredmedia'] ? '<div class="card-image"><img src="' + post._embedded['wp:featuredmedia']['0'].source_url + '" alt="" style="height: 100%;"></div>' : '';
+                    $(".posts").append('<div class="col s12 m6">\n' +
+                        '\t\t\t    <div class="card darken-1">\n' +
+                        post_image +
+                        '\t\t\t\t    <div class="card-content white-text">\n' +
+                        '\t\t\t\t\t    <span class="card-title">' + post.title.rendered + '</span>\n' +
+                        '\t\t\t\t\t    <p>' + post.excerpt.rendered + '</p>\n' +
+                        '\t\t\t\t    </div>\n' +
+                        '\t\t\t\t    <div class="card-action">\n' +
+                        '\t\t\t\t\t    <a href="https://zahidefe.net/?p=' + post.link + '">Yazıyı Oku</a>\n' +
+                        '\t\t\t\t    </div>\n' +
+                        '\t\t\t    </div>\n' +
+                        '\t\t    </div>');
+                })
+            }
+        });
+    });
+})
---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="assets/css/materialize.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+    <title>Yazı Arayın</title>
+</head>
+<body>
+<div class="container">
+    <div class="sw">
+        <form id="search-form">
+            <input type="search" class="search" placeholder="Search..."/>
+            <button class="go search-button"><i class="fa fa-search"></i></button>
+            <a href="https://zahidefe.net" class="logo" title="">
+                <img src="/assets/img/logo.png" alt="Zahidefe.net">
+            </a>
+        </form>
+    </div>
+    <div class="content">
+	    <div class="row posts">
+<!--		    <div class="col s12 m6">-->
+<!--			    <div class="card darken-1">-->
+<!--                    <div class="card-image">-->
+<!--                        <img src="/assets/img/placeholder.jpg" alt="" style="height: 100%;">-->
+<!--                    </div>-->
+<!--				    <div class="card-content white-text">-->
+<!--					    <span class="card-title" style="text-align: left;">Card Title</span>-->
+<!--					    <p style="text-align: left;">I am a very simple card. I am good at containing small bits of information.-->
+<!--						    I am convenient because I require little markup to use effectively.</p>-->
+<!--				    </div>-->
+<!--				    <div class="card-action" style="text-align: left;">-->
+<!--					    <a href="#">Yazıyı Oku</a>-->
+<!--				    </div>-->
+<!--			    </div>-->
+<!--		    </div>-->
+            <img src="assets/img/loading.svg" alt="Yükleniyor">
+	    </div>
+    </div>
+</div>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/custom.js"></script>
+</body>
+</html>
